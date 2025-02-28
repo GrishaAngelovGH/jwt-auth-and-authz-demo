dfd9220096e7e7985f6c17d25b7b132dc947532c
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-28 13:36:21 +0200

    client: Send the user credentials to the login rest service when the login form is submitted

--- client/src/components/pages/LoginPage/LoginPage.jsx
+++ client/src/components/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,91 @@
+import { useState } from "react"
+
 import "./LoginPage.css"
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("user@example.com")
+  const [password, setPassword] = useState("password123")
+
+  const handleSubmitForm = async e => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+
+    /*
+      When using FormData directly in a fetch request, the data is sent in a multipart/form-data format. 
+      This format is typically used for file uploads and more complex forms. 
+      However, if the server is expecting JSON data, it won"t parse multipart/form-data by default, 
+      which is why the fields were missing from req.body.
+    
+      By converting the FormData to a JSON object and setting the Content-Type to application/json, 
+      we align the data format with what the server expects, ensuring that the request body is parsed correctly.
+    */
+
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+
+      if (response.ok) {
+        alert("Successful Login!")
+      } else {
+        /*
+          When a client receives a 401 Unauthorized response, the catch block is typically called 
+          if there is a network error or an exception during the fetch request. 
+          However, a 401 response itself is not considered a network error—it is a valid HTTP 
+          response indicating that the request was not authorized.
+        */
+
+        const { error } = await response.json()
+        alert(error)
+      }
+    } catch (error) {
+      console.error("Error:", error)
+    }
+  }
+
+  const handleEmailChange = ({ target: { value } }) => {
+    setEmail(value)
+  }
+
+  const handlePasswordChange = ({ target: { value } }) => {
+    setPassword(value)
+  }
+
   return (
     <div className="page">
-      <form>
+      <form onSubmit={handleSubmitForm}>
         <label>
           <div>Email</div>
-          <input type="email" name="email" placeholder="Enter Email" className="form-control" required />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter Email"
+            className="form-control"
+            onChange={handleEmailChange}
+            required
+          />
         </label>
         <label>
           <div>Password</div>
-          <input type="password" name="password" placeholder="Enter Password" className="form-control" required />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter Password"
+            className="form-control"
+            onChange={handlePasswordChange}
+            required
+          />
         </label>
 
         <button type="submit">Log In</button>
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require('cors')
 const bodyParser = require("body-parser")
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 const port = 3000
 
@@ -16,7 +18,7 @@ app.post("/login", (req, res) => {
   const user = users.find(u => u.email === email && u.password === password)
 
   if (user) {
-    res.status(200)
+    res.status(200).json({ message: "Login successful" })
   } else {
     res.status(401).json({ error: "Invalid email or password" })
   }
