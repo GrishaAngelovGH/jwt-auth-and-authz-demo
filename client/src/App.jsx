6ecce8fe6f4777bcd5aca70f4a0fbf8e7175f739
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-28 11:53:21 +0200

    client: Integrate react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Login Page</div>} />
+      </Routes>
+    </Router>
   )
 }
 
