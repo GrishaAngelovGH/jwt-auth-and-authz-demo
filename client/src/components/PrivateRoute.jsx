31f4e923e85225155302c4cc3b15cd5826b52caa
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 14:32:15 +0200

    client: Validate token for authenticity and expiration in PrivateRoute and handle automatic logout

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,26 +1,45 @@
-import { useContext } from "react"
-
+import { useContext, useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { jwtDecode } from "jwt-decode"
 import AuthContext from "context/AuthContext"
 
-import { Navigate } from "react-router-dom"
+/*
+This implementation performs a series of essential checks: 
+- it verifies if a valid and non-expired token is present
 
-import { jwtDecode } from "jwt-decode"
+- triggers a logout action and invalidates the session if the token is expired
+
+- dynamically redirects unauthorized or logged-out users to the login page 
+  for secure route protection.
+*/
 
 const PrivateRoute = ({ children }) => {
+  const [isTokenValid, setIsTokenValid] = useState(true)
+
   const { auth, logout } = useContext(AuthContext)
 
-  const isTokenExpired = (token) => {
-    const { exp } = jwtDecode(token)
-    return Date.now() >= exp * 1000
-  }
+  const location = useLocation()
 
-  if (auth && isTokenExpired(auth)) {
-    alert("Your session has expired. Please log in again.")
-    logout()
+  useEffect(() => {
+    if (auth) {
+      const isTokenExpired = (token) => {
+        const { exp } = jwtDecode(token)
+        return Date.now() >= exp * 1000
+      };
+
+      if (isTokenExpired(auth)) {
+        alert("Your session has expired. Please log in again.")
+        logout()
+        setIsTokenValid(false)
+      }
+    }
+  }, [auth, location])
+
+  if (!auth || !isTokenValid) {
     return <Navigate to="/" />
   }
 
-  return auth ? children : <Navigate to="/" />
+  return children
 }
 
 export default PrivateRoute
