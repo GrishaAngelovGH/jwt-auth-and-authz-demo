d9efe8c7b90135ce3f4003eb08d9c8fc0b3d8320
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 11:02:16 +0200

    client: Add logout button in the home page

--- client/src/components/pages/HomePage/HomePage.jsx
+++ client/src/components/pages/HomePage/HomePage.jsx
@@ -1,6 +1,31 @@
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+
+import AuthContext from "context/AuthContext"
+
+import "./HomePage.css"
+
 const HomePage = () => {
+  const { logout } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate("/")
+  }
+
   return (
-    <div>HomePage</div>
+    <div className="home-page">
+      <h2>Home Page</h2>
+
+      <div className="greet">
+        You are successfully logged in!
+      </div>
+
+      <div className="buttons">
+        <button className="logout-btn" onClick={handleLogout}>Log Out</button>
+      </div>
+    </div>
   )
 }
 
