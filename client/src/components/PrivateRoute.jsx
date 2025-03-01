eb57c58e485a57e2359a3d924c47082abd6cd480
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 12:31:16 +0200

    client: Add check for token expiration in the private route component

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,11 +1,24 @@
-import { Navigate } from "react-router-dom"
-
 import { useContext } from "react"
 
 import AuthContext from "context/AuthContext"
 
+import { Navigate } from "react-router-dom"
+
+import { jwtDecode } from "jwt-decode"
+
 const PrivateRoute = ({ children }) => {
-  const { auth } = useContext(AuthContext)
+  const { auth, logout } = useContext(AuthContext)
+
+  const isTokenExpired = (token) => {
+    const { exp } = jwtDecode(token)
+    return Date.now() >= exp * 1000
+  }
+
+  if (auth && isTokenExpired(auth)) {
+    alert("Your session has expired. Please log in again.")
+    logout()
+    return <Navigate to="/" />
+  }
 
   return auth ? children : <Navigate to="/" />
 }
