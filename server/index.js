cc6a18384f876013177270e0981780d1d9333874
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 09:53:02 +0200

    server: Generate new JSON web token and send it to the client when the login is successful

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
-const cors = require('cors')
+const cors = require("cors")
 const bodyParser = require("body-parser")
+const jwt = require("jsonwebtoken")
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 const port = 3000
+
+const SECRET_KEY = "SECRET_KEY_12345"
 
 const users = [
   { id: 1, email: "user@example.com", password: "password123" }
@@ -18,7 +21,18 @@ app.post("/login", (req, res) => {
   const user = users.find(u => u.email === email && u.password === password)
 
   if (user) {
-    res.status(200).json({ message: "Login successful" })
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email
+      },
+      SECRET_KEY,
+      {
+        expiresIn: '1m'
+      }
+    )
+
+    res.status(200).json({ token })
   } else {
     res.status(401).json({ error: "Invalid email or password" })
   }
