24ff736c19a1e9b12836536d3b3ae3343deba407
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 10:06:49 +0200

    client: Add home page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom"
 
 import LoginPage from "components/pages/LoginPage"
+import HomePage from "components/pages/HomePage/HomePage"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   )
