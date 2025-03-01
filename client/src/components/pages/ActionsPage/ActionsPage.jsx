bb1f1cf123c4220765e8eee971568fe615d83325
Grisha Angelov <grisha.angelov89@gmail.com>
2025-03-01 14:06:20 +0200

    "client: Make request to secure backend endpoint when performing action on the actions page

--- client/src/components/pages/ActionsPage/ActionsPage.jsx
+++ client/src/components/pages/ActionsPage/ActionsPage.jsx
@@ -1,6 +1,42 @@
+import { useState, useContext } from "react"
+
+import AuthContext from "context/AuthContext"
+
 const ActionsPage = () => {
+  const [message, setMessage] = useState("")
+  const { auth } = useContext(AuthContext)
+
+  const fetchProtectedData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/action", {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${auth}`,
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (response.ok) {
+        const { message, user } = await response.json()
+        setMessage(`${user.email}: ${message}`)
+      } else {
+        console.error("Error:", response.statusText)
+      }
+    } catch (error) {
+      console.error("Network Error:", error)
+    }
+  }
+
   return (
-    <div>ActionsPage</div>
+    <div className="page flex-column">
+      <h1>Actions Page</h1>
+      <button onClick={fetchProtectedData}>Perform Action</button>
+      {
+        message.length > 0 && (
+          <p>{message}</p>
+        )
+      }
+    </div>
   )
 }
 
